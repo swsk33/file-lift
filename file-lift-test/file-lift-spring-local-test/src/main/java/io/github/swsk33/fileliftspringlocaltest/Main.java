9375aff25c10f7662cadd4e613deb4959f01a325
守望时空33 <yanhanhui2001a@163.com>
2023-06-29 17:49:36 +0800

    完成部分文档

---
+++ file-lift-test/file-lift-spring-local-test/src/main/java/io/github/swsk33/fileliftspringlocaltest/Main.java
@@ -0,0 +1,15 @@
+package io.github.swsk33.fileliftspringlocaltest;
+
+import io.github.swsk33.fileliftcore.service.UploadFileService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("mongodb.xml");
+		UploadFileService service = context.getBean(UploadFileService.class);
+		System.out.println(service);
+	}
+
+}
